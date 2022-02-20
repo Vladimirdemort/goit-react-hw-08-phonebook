62f2bd4e33b4ad79315423542fb50e43ee86d99d
Volodymyr Dvorskyi <feowurd@gmail.com>
2022-02-20 14:14:04 +0200

    created app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Loader from 'react-loader-spinner';
+import Container from './components/Container';
+import AppBar from './components/AppBar/AppBar';
+import errorImage from './pages/error.jpg';
 import './App.css';
 
-function App() {
+const HomeView = lazy(() =>
+  import('./pages/HomeView.jsx' /* webpackChunkName: "HomeView" */),
+);
+const NotFoundView = lazy(() =>
+  import('./pages/NotFoundView.jsx' /* webpackChunkName: "NotFoundView" */),
+);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container title="Hello world!">
+      <AppBar />
+
+      <Suspense
+        fallback={
+          <Loader
+            type="Puff"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={3000}
+          />
+        }
+      >
+        <Routes>
+          <Route path="" element={<HomeView />} />
+          <Route
+            path="*"
+            element={
+              <NotFoundView
+                errorImage={errorImage}
+                messadge="Ошибка 404: страница не найдена :("
+              />
+            }
+          />
+        </Routes>
+      </Suspense>
+
+      <ToastContainer />
+    </Container>
   );
 }
-
-export default App;
