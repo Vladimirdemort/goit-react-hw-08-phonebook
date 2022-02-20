f636acc14d9e2c993ffcba72d127e33740553adb
Volodymyr Dvorskyi <feowurd@gmail.com>
2022-02-20 14:27:40 +0200

    test commit

--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -5,27 +5,7 @@ export default function HomeView() {
     <>
       <PageHeading text="Home title" />
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex vel velit
-        nihil illo est! Quos cum rerum dolores voluptates odio iste est nam.
-      </p>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex vel velit
-        nihil illo est! Quos cum rerum dolores voluptates odio iste est .
-      </p>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex vel velit
-        nihil illo est! Quos cum rerum dolores voluptates odio iste est nam
-        excepturi .
-      </p>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex vel velit
-        nihil illo est! Quos cum rerum dolores voluptates odio iste est nam
-        excepturi placeat .
-      </p>
     </>
   );
 }
